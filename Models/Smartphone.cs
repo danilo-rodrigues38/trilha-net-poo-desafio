12bfa266c180dfc612ffe50bebd93a2b07bfebea
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2024-04-24 22:57:01 -0300

    Update Smartphone.cs

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -6,7 +6,6 @@ namespace DesafioPOO.Models
         private string Modelo { get; set; }
         private string IMei { get; set; }
         private int Memoria { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
         public Smartphone(string numero, string modelo, string imei, int memoria)
         {
